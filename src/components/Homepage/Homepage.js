46ef0f4353ea482dff1fd873c824ad39c45b894b
DevWichrowski <wichrowski.patryk92@gmail.com>
2020-02-13 16:30:09 +0100

    Delete layout component

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import PlanetList from "./PlanetList/PlanetList";
-import Header from "../shared/Header/Header";
-import Footer from "../shared/Footer/Footer";
 
 
 const Homepage = () => {
     return (
         <div>
-            <Header/>
             <PlanetList/>
-            <Footer/>
         </div>
     );
 };
